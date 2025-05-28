897d34c6fae86640d26157f7091e66c38c7ad4f9
Pavel Drovnin <pavel98rus@gmail.com>
2025-05-29 02:29:21 +0300

    schemas

---
+++ app/schemas/query.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel
+
+
+class CityQueryBase(SQLModel):
+    csrftoken: str = Field(max_length=100)
+    timestamp: datetime | None = Field(default=datetime.now(), nullable=False)
+    city: str = Field(max_length=30)
+
+
+class CityQuery(CityQueryBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class CityQueryCreate(CityQueryBase):
+    pass
+
+
+class CityQueryRead(CityQueryBase):
+    pass
+
+
+class CityStats(SQLModel):
+    city: str
+    count: int
+
+
+class CityStatsRead(SQLModel):
+    stats: list[CityStats]
